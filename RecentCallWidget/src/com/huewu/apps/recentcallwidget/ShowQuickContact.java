55b8dae4661e9c0c05ed86272ef195b9f44873b1
huewu.yang <huewu.yang@f90c7e65-7878-734d-4c3a-9306a136ce14>
2010-12-02 14:42:05 +0000

    integrate with quickcontact feature

--- RecentCallWidget/src/com/huewu/apps/recentcallwidget/ShowQuickContact.java
+++ RecentCallWidget/src/com/huewu/apps/recentcallwidget/ShowQuickContact.java
@@ -1,20 +1,59 @@
 package com.huewu.apps.recentcallwidget;
 
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.PhoneLookup;
 import android.provider.ContactsContract.QuickContact;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.widget.QuickContactBadge;
 import android.widget.TextView;
 
 public class ShowQuickContact extends Activity{
 	
+	Uri lookupUri = null;
+	boolean finish = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		TextView tv = new TextView(this);
+		tv.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+		tv.setBackgroundColor(Color.TRANSPARENT);
 		setContentView(tv);
 
-		Uri uri = Uri.parse("content://com.android.contacts/contacts/lookup/2508i1/7");
-		QuickContact.showQuickContact(this, tv,uri, QuickContact.MODE_LARGE, null);
+		//get content uri from call log.
+
+		Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode("2222222"));
+		Cursor c = managedQuery(uri, null, null, null, null);
+		String lookup = null;
+		while(c.moveToNext() == true){
+			lookup = c.getString(c.getColumnIndex(PhoneLookup.LOOKUP_KEY));
+		}
+		c.close();
+
+		if(lookup == null){
+			Uri createUri = Uri.fromParts("tel", "2222222", null);
+			Intent intent = new Intent(ContactsContract.Intents.SHOW_OR_CREATE_CONTACT, createUri);
+            startActivity(intent);
+		}else{
+			lookupUri = Uri.withAppendedPath(Contacts.CONTENT_LOOKUP_URI, lookup);
+			QuickContact.showQuickContact(this, tv,lookupUri, QuickContact.MODE_LARGE, null);
+		}
+		finish();
 	}
 }//end of class
