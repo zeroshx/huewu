883efec7d1382a4303fc2f2704f550b6598566a1
huewu.yang <huewu.yang@f90c7e65-7878-734d-4c3a-9306a136ce14>
2010-12-11 16:48:33 +0000

    add comments.

--- RecentContactsWidget/src/com/huewu/apps/recentcontactswidget/RecentContactsManager.java
+++ RecentContactsWidget/src/com/huewu/apps/recentcontactswidget/RecentContactsManager.java
@@ -1,11 +1,5 @@
 package com.huewu.apps.recentcontactswidget;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.huewu.apps.recentcontactswidget.R;
-import com.huewu.apps.recentcontactswidget.RecentContactsManager.SimpleContact;
-
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -13,19 +7,14 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
 import android.net.Uri;
-import android.provider.CallLog;
 import android.provider.ContactsContract;
-import android.provider.ContactsContract.PhoneLookup;
-import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.util.Log;
-import android.view.View;
 import android.widget.RemoteViews;
 
 /**
  * <p>
- * @file			RecentCallWidgetManager.java
- * @version			1.0
+ * @file			RecentContactstManager.java
+ * @version			1.1
  * @date 			Dec. 5, 2010
  * @author 			huewu.yang
  * <p>
@@ -49,28 +38,32 @@ public class RecentContactsManager {
 	final static int MODE_STILL_WIDGET = 1001;
 	final static int MODE_UP_SCROLLING_WIDGET = 1002;
 	final static int MODE_DOWN_SCROLLING_WIDGET = 1003;
-	final static int MAX_CONTACTS_COUNT = 10;
-	final static int DISPLAY_CONTACTS_COUNT = 3;
-	final static int LIST_CONTACTS_COUNT = 5;	//include 2 padding(for animation) list.
+	final static int MAX_CONTACTS_COUNT = 10;	//max number of list, which can browse by recent contacts widget.
+	final static int DISPLAY_CONTACTS_COUNT = 3;	//number of list, which an user actually see and select.
+	final static int LIST_CONTACTS_COUNT = 5;	//DISPLAY_CONTACTS_COUNT + padding(for animation effect) list.
 	
 	final static int SCROLL_UP = 2001;
 	final static int SCROLL_DOWN = 2002;
 	final static int SCROLL_NO = 2003;
 
+	/**
+	 * public constructor.
+	 * @param context
+	 */
 	public RecentContactsManager(Context context){
 		mAppContext = context.getApplicationContext();
 		mContactsManager = new ContactsManager();
 		
-		//load current index.
+		//load current list index. default value is 0.
 		SharedPreferences preference = mAppContext.getSharedPreferences(WIDGET_PREFERENCE, 0);
 		mIndex = preference.getInt(CURRENT_INDEX, 0);
 	}
 
 	/**
 	 * create a recent call widget remote view.
-	 * still widget - (not move widget)
-	 * scroll up widget - (scroll up animation widget)
-	 * scroll down widget - (scroll down animation widget)
+	 * still widget - MODE_STILL_WIDGET (non-moving widget)
+	 * scroll up widget - MODE_UP_SCROLLING_WIDGET - (scroll up animation widget)
+	 * scroll down widget - MODE_DOWN_SCROLLING_WIDGET - (scroll down animation widget)
 	 * @param context
 	 * @param mode one of values : MODE_STILL_WIDGET / MODE_UP_SCROLLING_WIDGET / MODE_DOWN_SCROLLING_WIDGET
 	 * @return created remote view.
@@ -81,7 +74,7 @@ public class RecentContactsManager {
 		//#1. get recent call logs.
 		 SimpleContact[] contacts = mContactsManager.getRecentContactDisplayName(MAX_CONTACTS_COUNT);
 		
-		if(contacts.length - DISPLAY_CONTACTS_COUNT < mIndex)
+		if(contacts.length - DISPLAY_CONTACTS_COUNT < mIndex)	//too few contacts to scroll in the list.
 			mIndex = 0;	//reset index.
 
 		//#2. build a proper remote view.
@@ -105,6 +98,12 @@ public class RecentContactsManager {
 		return views;
 	}
 	
+	/**
+	 * Android (at least in the nexus1) doesn't mark the contact as contacted when receiving sms, so do it manually.
+	 * #1.convert pdusObj to SmsMessage.
+	 * #2.mark the senders' contact(if it contains in the contacts) as contacted.
+	 * @param pdusObj
+	 */
 	public void markContacted(Object[] pdusObj) {
 		if(pdusObj == null)
 			return;
@@ -117,16 +116,24 @@ public class RecentContactsManager {
 	}
 
 	private void applyScroll(RemoteViews views, SimpleContact[] contacts, int move) {
+		
+		//scroll animation works as below.
+		//#1.In the parent LinearLayout, there are five buttons which have exactly same height( !!using layout_weight attribute. )
+		//#2.3 buttons are displaying buttons, the others are padding buttons.
+		//#3.when we scroll up the list. ++ or -- starting index, and set text value.
+		//#4.so, at the padding button, the text value of original index's button is cloned.
+		//#5.start layout animation, which move up or down all buttons by 20% (cloned text is showing, so user can't notice).
+		//#6.animation is playing and all the buttons position is moving to original position by y-axis. (so, the scrolling animation is showing!)
 
 		switch(move){
 		case SCROLL_NO:
-//			views.setViewVisibility(mCallItemList[0], View.INVISIBLE);	//hide		
-//			views.setViewVisibility(mCallItemList[4], View.INVISIBLE);	//hide		
+			//do nothing.
 			break;
 		case SCROLL_UP:
-			if(mIndex < contacts.length - 3){
+			if(mIndex < contacts.length - DISPLAY_CONTACTS_COUNT){
 				++mIndex;
 				try{
+					//clone text value.
 					views.setTextViewText(mCallItemList[0], contacts[mIndex-1].mDisplayName);	//hide		
 				}catch(Exception e){
 				}				
@@ -136,6 +143,7 @@ public class RecentContactsManager {
 			if(mIndex > 0){
 				--mIndex;
 				try{
+					//clone text value.
 					views.setTextViewText(mCallItemList[4], contacts[mIndex+3].mDisplayName);	//hide		
 				}catch(Exception e){
 				}
@@ -144,19 +152,19 @@ public class RecentContactsManager {
 		}
 
 		if(mIndex > 0){
-			//scroll down is possible.
+			//scroll down is possible. set pending intent to the scroll button.
 			Intent down = new Intent(RecentContactsWidget.ACTION_SCROLL_DOWN);
 			PendingIntent pendingDown 
 				= PendingIntent.getBroadcast(mAppContext, 0, down, PendingIntent.FLAG_CANCEL_CURRENT);
-			views.setOnClickPendingIntent(R.id.scrollDown, pendingDown);
+			views.setOnClickPendingIntent(R.id.scrollUp, pendingDown);
 		}
 		
 		if(mIndex < contacts.length - 3){
-			//scroll up is possible.
+			//scroll up is possible. set pending intent to the scroll button.
 			Intent up = new Intent(RecentContactsWidget.ACTION_SCROLL_UP);			
 			PendingIntent pendingUp 
 				= PendingIntent.getBroadcast(mAppContext, 0, up, PendingIntent.FLAG_CANCEL_CURRENT);
-			views.setOnClickPendingIntent(R.id.scrollUp, pendingUp);
+			views.setOnClickPendingIntent(R.id.scrollDown, pendingUp);
 		}
 		
 		//save current index.
@@ -170,26 +178,26 @@ public class RecentContactsManager {
 		if(views == null)
 			return;
 
+		//mIndex is starting index of displayed contact item list.
 		int count = ( (contacts.length - mIndex) < DISPLAY_CONTACTS_COUNT ) ? (contacts.length - mIndex) : DISPLAY_CONTACTS_COUNT;
 
 		int index = 0;
 		for(index = 0; index <count; ++index){
 			String name = contacts[mIndex + index].mDisplayName;
 			Uri uri = contacts[mIndex + index].mUri;
-			views.setTextViewText(mCallItemList[index + 1], name);	
+			views.setTextViewText(mCallItemList[index + 1], name);
+			//pending intent which has the same action name can't copied. so use different action name for each button.
 			Intent intent = new Intent(mCallIntentList[index]);
 			intent.setData(uri);
 			intent.putExtra(ContactsContract.Contacts.DISPLAY_NAME, name);
 			PendingIntent pendingCall = PendingIntent.getActivity(mAppContext, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 			views.setOnClickPendingIntent(mCallItemList[index + 1], pendingCall);			
 		}
-
-//		for(index = index; index < mCallItemList.length - 2; ++ index){
-//			views.setViewVisibility(mCallItemList[index + 1], View.INVISIBLE);	//hide
-//		}
 	}
 	
-	
+	/**
+	 * simple data call which represents one specific ContactsContract.
+	 */
 	class SimpleContact {
 		String mDisplayName;
 		Uri mUri;
@@ -199,22 +207,27 @@ public class RecentContactsManager {
 		}
 	}
 
+	/**
+	 * inner class which deals with ContactsContract Provider.
+	 */
 	class ContactsManager {
 
-		public SimpleContact[] getRecentContactDisplayName(int count){
+		SimpleContact[] getRecentContactDisplayName(int count){
 			
 			Cursor c = mAppContext.getContentResolver().query(
 					ContactsContract.Contacts.CONTENT_URI, 
 					new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.LOOKUP_KEY}, 
 					null, 
 					null, 
-					ContactsContract.Contacts.LAST_TIME_CONTACTED + " DESC");
+					ContactsContract.Contacts.LAST_TIME_CONTACTED + " DESC");	//sort by LAST_TIME_CONTACTED.
 
 			if(c == null){
+				//error happen.
 				return new SimpleContact[0];
 			}
 
 			int ct = 0;
+			//maximum list size is given integer count.
 			int listSize = (count < c.getCount()) ? count : c.getCount();
 
 			SimpleContact[] result = new SimpleContact[listSize];
@@ -227,7 +240,7 @@ public class RecentContactsManager {
 				SimpleContact contact = new SimpleContact(name, uri);
 				result[ct] = contact;
 				++ct;
-				if(ct == listSize)	//fill list.
+				if(ct == listSize)	//list is full.
 					break;
 			}
 			c.close();
@@ -235,11 +248,17 @@ public class RecentContactsManager {
 			return result;
 		}
 
-		public void markContacted(String originatingAddress) {
+		void markContacted(String originatingAddress) {
+			//#1. select ContactsContract information by using phone number.
 			Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(originatingAddress));
 			Cursor c = mAppContext.getContentResolver().query(
 					uri, null, null, null, null);
+
+			if(c == null)	//error happen
+				return;
+			
 			while(c.moveToNext() == true){
+				//only if the originatingAddress is stored in the contacts.
 				long id = c.getLong(c.getColumnIndex(ContactsContract.PhoneLookup._ID));
 				ContactsContract.Contacts.markAsContacted(mAppContext.getContentResolver(), id);
 			}
@@ -249,7 +268,6 @@ public class RecentContactsManager {
 
 	private static String WIDGET_PREFERENCE = "setting";
 	private static String CURRENT_INDEX = "index";
-	
 	
 	private Context mAppContext = null;
 	private ContactsManager mContactsManager = null;
